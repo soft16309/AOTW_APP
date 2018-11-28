3880c97a6b6d6c1c123645c3eaa4cfb085eca736
WisdomZhi <38318654+WisdomZhi@users.noreply.github.com>
2018-11-28 10:52:14 +0800

    更正serverUrl.js

--- AOTW/js/serverUrl.js
+++ AOTW/js/serverUrl.js
@@ -1,2 +1,4 @@
 //var contextPath = "http://192.168.56.1:8080/AOTW";
+//var wsContextPath = "ws://192.168.56.1:8080/AOTW";
 var contextPath = "http://gdqy.soft16309.aotw.ngrok.xiaomiqiu.cn/AOTW";
+var wsContextPath = "ws://gdqy.soft16309.aotw.ngrok.xiaomiqiu.cn/AOTW";
