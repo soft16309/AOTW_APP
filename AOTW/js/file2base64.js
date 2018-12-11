81ccfb07f09a23edf5dc9cb5dd9b8ba79dd80854
WisdomZhi <38318654+WisdomZhi@users.noreply.github.com>
2018-12-11 19:58:27 +0800

    修复了相机无法扫描，并增加头像裁剪功能

--- AOTW/js/file2base64.js
+++ AOTW/js/file2base64.js
@@ -98,3 +98,16 @@ function compressImage(path, callback){
 	}
 
 }
+
+//base64转换为二进制文件
+function dataURLToBlob(dataurl){
+	var arr = dataurl.split(',');
+	var mime = arr[0].match(/:(.*?);/)[1];
+	var bstr = atob(arr[1]);		//base64解码
+	var n = bstr.length;
+	var u8arr = new Uint8Array(n);
+	while(n--){
+		u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new Blob([u8arr], {type:mime});
+}
